1f2f3f29b3371ef409bed12e7dbb79fcb365c4f1
viclop1597 <victor.rodi25@gmail.com>
2023-04-17 18:15:17 -0600

    changes

--- exercise2.java
+++ exercise2.java
@@ -4,28 +4,18 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class exercise2 {
-
     public static void main(String[] args) {
-        
         String fileName = "first_names.txt"; // name of the file containing the first names
-        
-        // read the names from the file and store them in an array
-        String[] names = readNamesFromFile(fileName);
-        
-        // sort the names in alphabetical order
-        Arrays.sort(names);
-        
+        String[] names = readNamesFromFile(fileName); // read the names from the file and store them in an array
+        Arrays.sort(names); // sort the names in alphabetical order
         int totalScore = 0; // variable to store the total score
-        
-        // calculate the score for each name and add it to the total score
         for (int i = 0; i < names.length; i++) {
-            int nameScore = calculateExercise1(names[i], i+1);
+            int nameScore = calculateNameScore(names[i], i + 1); // calculate the score for each name and add it to the total score
             totalScore += nameScore;
         }
-        
         System.out.println("Total score of all the names in the file: " + totalScore);
     }
-    
+
     /**
      * Reads the first names from the given file and returns them in an array
      * @param fileName name of the file containing the first names
@@ -36,8 +26,8 @@ public class exercise2 {
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = br.readLine()) != null) {
-                // split the line into names separated by commas
-                String[] splitNames = line.split(",");
+                // remove the quotes and split the line into names separated by commas
+                String[] splitNames = line.replace("\"", "").split(",");
                 // add each name to the array
                 names = Arrays.copyOf(names, names.length + splitNames.length);
                 System.arraycopy(splitNames, 0, names, names.length - splitNames.length, splitNames.length);
@@ -47,14 +37,14 @@ public class exercise2 {
         }
         return names;
     }
-    
+
     /**
      * Calculates the score for the given name
-     * @param name name for which to calculate the score 
+     * @param name name for which to calculate the score
      * @param position position of the name in the sorted array of names
      * @return score for the given name
      */
-    private static int calculateExercise1(String name, int position) {
+    private static int calculateNameScore(String name, int position) {
         int score = 0;
         for (int i = 0; i < name.length(); i++) {
             char c = name.charAt(i);
